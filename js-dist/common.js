817f8eed03e251dddf10f2a6a7a5aa9621b25467
Yurii <yurayurakh@gmail.com>
2017-10-31 00:21:10 +0200

    finish

--- js-dist/common.js
+++ js-dist/common.js
@@ -6,6 +6,27 @@ $(document).ready(function() {
         }else {
             $("#search-block").toggleClass('search-open').fadeIn(300);
         }
-
     });
+
+    //Mobile
+    if(screen.width < 1200)
+    {
+
+        //Hamburger menu
+        $( ".cross" ).hide();
+        $( ".nav" ).hide();
+        $( ".hamburger" ).click(function() {
+            $( ".nav" ).slideToggle( 200, function() {
+                $( ".hamburger" ).hide(300);
+                $( ".cross" ).show(300);
+            });
+        });
+
+        $( ".cross" ).click(function() {
+            $( ".nav" ).slideToggle( 200, function() {
+                $( ".cross" ).hide(300);
+                $( ".hamburger" ).show(300);
+            });
+        });
+    }
 });
